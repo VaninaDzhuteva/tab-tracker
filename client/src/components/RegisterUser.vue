34dde5f1ae21d9a182f8b5889551cea13bac6e8f
Vanina Mikova <vanina.dzhuteva@gmail.com>
2022-05-02 12:18:21 +0300

    Add database configuration. Set up registratrion form

--- client/src/components/RegisterUser.vue
+++ client/src/components/RegisterUser.vue
@@ -1,13 +1,42 @@
 <template>
-    <div>
-        <h1>Register</h1>
-
-        <input type="email" name="email" placeholder="email" v-model="email">
-
-        <input type="password" name="password" placeholder="password" v-model="password">
-
-        <button @click="registerUser">Register</button>
-    </div>
+    <v-layout column>
+    <v-container>
+        <v-flex >
+            <div class="white mx-auto">
+            <!-- <v-toolbar flat dense class="cyan" dark>
+                <v-toolbar-title>Register</v-toolbar-title>
+            </v-toolbar> -->
+        
+            <v-card title="Register" max-width="700" class="mx-auto">
+                <form 
+                name="tab-tracker-form"
+                autocomplete="off">
+                <v-text-field
+                    label="Email"
+                    v-model="email"
+                ></v-text-field>
+                <br>
+                <v-text-field
+                    label="Password"
+                    type="password"
+                    v-model="password"
+                    autocomplete="new-password"
+                ></v-text-field>
+                </form>
+                <br>
+                <div class="danger-alert" v-html="error" />
+                <br>
+                <v-btn
+                    dark
+                    class="primary"
+                    @click="register">
+                    Register
+                </v-btn>
+            </v-card>
+            </div>
+        </v-flex>
+    </v-container>
+  </v-layout>
 </template>
 
 <script>
@@ -16,19 +45,31 @@ export default {
     data() {
         return {
             email: '',
-            password: ''
+            password: '',
+            error: null
         }
     },
 
     methods: {
         async registerUser() {
-            const response = await AuthenticationServise.register({
-                email: this.email,
-                password: this.password
-            })
-            console.log(response.data);
+            try {
+                await AuthenticationServise.register({
+                    email: this.email,
+                    password: this.password
+                })
+            } catch (error) {
+                this.error = error.response.data.error
+            }
+
+           
         }
     }
 
 }
 </script>
+
+<style>
+.error {
+    color: red;
+}
+</style>
